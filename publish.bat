b09aaa5cad02b517bef421d6132a0153d0be8677
Tobias Koppers <tobias.koppers@googlemail.com>
2012-09-14 00:37:17 +0200

    fixed publish logic

--- publish.bat
+++ publish.bat
@@ -3,6 +3,7 @@ git checkout gh-pages
 git merge master
 rmdir /Q /S assets
 node _template\publish
+git add index.html assets
 git add index.html assets -u
 git commit -m "Publish"
 git checkout master
--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@ git checkout gh-pages
 git merge master
 rm -rf assets
 node _template/publish
+git add index.html assets
 git add index.html assets -u
 git commit -m "Publish"
 git checkout master
